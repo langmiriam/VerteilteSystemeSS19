7d2feda989c80f344863232bd3bb005c7480ae2b
Marcel Schneider <Marcel_Fabien.Schneider@Student.Reutlingen-University.de>
2019-05-31 02:07:56 +0200

    The Order Service now works. Added multiple methods and added a class.

--- orderService/order-service/src/main/java/com/mimilang/orderservice/controller/OrderController.java
+++ orderService/order-service/src/main/java/com/mimilang/orderservice/controller/OrderController.java
@@ -2,46 +2,59 @@ package com.mimilang.orderservice.controller;
 
 
 import com.mimilang.orderservice.entities.OrderItem;
+import com.mimilang.orderservice.entities.Product;
 import com.mimilang.orderservice.service.OrderService;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RestController("/orders")
+@RestController
 public class OrderController {
 
-    OrderService orderService;
+    @Autowired
+    private OrderService orderService;
+
 
     //only for tests
     @RequestMapping("/hello")
-    public String getHelloOrder(){
-        return "Hello Order!";
+    public String getHello(){
+        return "The Order Service is running.";
     }
 
+
     //gets all orders
-    @RequestMapping("/")
+    @RequestMapping("/orders")
     public List<OrderItem> getAllOrders(){
         return orderService.getAllOrderItems();
     }
 
 
-    //gets one specific orders
-    @RequestMapping(value=("/customers/{id}"))
-    public OrderItem addOrderItem(@PathVariable("id") Long id){
-        return orderService.addOrderItem(id);
+    //gets one specific order
+    @RequestMapping(value="/orders/{id}", method = RequestMethod.GET)
+    public OrderItem getOrderItem(@PathVariable("id") String id){
+        return orderService.getOrderItem(Long.parseLong(id));
     }
 
 
+    //add an new order
+    @RequestMapping(value="/orders/{id}", method= RequestMethod.POST)
+    public String addOrder(@RequestBody Product product, @PathVariable("id") String id){
+        return orderService.addOrderItem(Long.parseLong(id), product);
+    }
 
 
+    //update an order
+    @RequestMapping(value="/orders/{id}", method= RequestMethod.PUT)
+    public String  addCustomer(@RequestBody Product product, @PathVariable String id){
+        return orderService.updateOrder(Long.parseLong(id), product);
+    }
 
 
-
-
-
-
+    //delete an order
+    @RequestMapping(value="/orders/{id}", method= RequestMethod.DELETE)
+    public String removeCustomer(@PathVariable String id){
+        return orderService.removeOrder(Long.parseLong(id));
+    }
 
 }
