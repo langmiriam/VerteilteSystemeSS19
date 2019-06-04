8bb17b1189a1218c94342725e9d0e57ea53ad5fe
AlexMPollok <alex.pollok1@gmail.com>
2019-06-04 13:56:49 +0200

    Added the Config Server App and directory for configuration files

---
+++ MensaConfigServer/src/test/java/Config/MensaConfigServer/MensaConfigServerApplicationTests.java
@@ -0,0 +1,16 @@
+package Config.MensaConfigServer;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MensaConfigServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
