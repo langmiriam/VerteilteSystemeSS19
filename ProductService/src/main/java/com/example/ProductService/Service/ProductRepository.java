97701e0fd8f1b0c5635d6400dff528e682f3c134
miriamLang1 <miriam.lang@student.reutlingen-university.de>
2019-06-08 12:54:34 +0200

    eureka server running, product delete

--- ProductService/src/main/java/com/example/ProductService/Service/ProductRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.ProductService.Service;
-
-import ProductService.Entities.Product;
-import org.springframework.data.repository.CrudRepository;
-
-public interface ProductRepository extends CrudRepository<Product, Long> {
-}
-
