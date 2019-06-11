22cdd39c9679451b4b8152c54d90d81c341a9cd2
Arthur Huber <arthur.huber@student.reutlingen-university.de>
2019-06-11 19:09:48 +0200

    Merge and write service names in lower case

--- customerService/customer-service/src/main/java/com/miriamLang1/customerservice/serviceImpl/CustomerRepository.java
+++
@@ -1,7 +0,0 @@
-package com.miriamLang1.customerservice.serviceImpl;
-
-import com.miriamLang1.customerservice.entity.Customer;
-import org.springframework.data.repository.CrudRepository;
-
-public interface CustomerRepository extends CrudRepository<Customer, Long> {
-}
